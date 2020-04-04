f7aaec55fef10772d9f188c0a4c20a617c7bab82
reza <reza_pisc@yahoo.com>
2020-04-04 18:15:11 +0430

    stock

---
+++ Molavi/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'Molavi'
+
+urlpatterns = [
+    #/categories/
+    path('categories/', views.categories,name='categories'),
+    #categories/id/
+    path('categories/<int:question_id>/raftare_manfi/', views.raftare_manfi, name='raftare_manfi'),
+    #path('categories/<int:question_id>/jameh/', views.jameh, name='jameh'),
+
+
+
+]
--- Molavi/views.py
+++ Molavi/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from . import models
 
-# Create your views here.
+
+def categories(request):
+    categories = models.categories.objects.all()
+    return render(request, 'Molavi/categories.html', {'categories': categories})
+
+
+def raftare_manfi(request, question_id):
+    raftare_manfi = get_object_or_404(models.categories, pk=question_id)
+    return render(request, 'Molavi/raftare_manfi.html', {'raftare_manfi': raftare_manfi})
+
+
+def jameh(request, question_id):
+    jameh = get_object_or_404(models.categories, pk=question_id)
+    return render(request, 'Molavi/jameh.html', {'jameh': jameh})
